8b149aee067e0b5ae17da3251ce27f572865b09e
Дегтярев Василий Александрович <vdegtyarev@cbgr.ru>
2024-01-28 19:16:19 +0500

    Lab 1 finish Add image generation for statistic data.

--- lab1/StatisticCalculator.py
+++ lab1/StatisticCalculator.py
@@ -1,20 +1,69 @@
+import math as mt
+
+
+def calculate_statistic_function(normilize_el, dx):
+    x = list([0])
+    y = list([0])
+
+    for i in range(1, len(normilize_el) + 1):
+        x.append(x[i - 1] + dx)
+        y.append(y[i - 1] + normilize_el[i - 1])
+        if i == len(normilize_el):
+            y[i] = round(y[i])
+    return x, y
+
+
+def calculate_empheric_rule(data, dx, n):
+    min_el, max_el = min(data), max(data)
+    counted_el = list()
+    bar_names = list()
+
+    x = min_el
+
+    for i in range(n):
+        left = x
+        right = x + dx
+        if i == (n - 1):
+            counted_el.append(sum(left <= y <= max_el for y in data))
+        else:
+            counted_el.append(sum(left <= y < right for y in data))
+
+        bar_names.append(str(round(x, 2)) +
+                         '\n - \n' +
+                         str(round(x + dx, 2)))
+        x += dx
+
+    normilize_el = [i / len(data) for i in counted_el]
+    return counted_el, normilize_el, bar_names
+
+
 def calculate_expected_value(data):
     print('Start calculate expected value')
-    return sum(data)/len(data)
+    return sum(data) / len(data)
+
 
 def calculate_dispersion(data, expected_value):
     print('Start calculate dispersion')
     dispersion = 0
     for a in data:
-        dispersion+=pow(a-expected_value, 2)
+        dispersion += pow(a - expected_value, 2)
 
-    return dispersion/(len(data)-1)
+    return dispersion / (len(data) - 1)
 
 
+def calculate_dx(data):
+    R = max(data) - min(data)
+
+    # Часто встречается записанным через десятичный логарифм:
+    # n = 1 + ⌊ 3.322 lg N ⌋
+    n = (1 + int(3.322 * mt.log(len(data), 10)))
+    dx = R / (1 + int(3.322 * mt.log(len(data), 10)))
+    return dx, n
 
 
 def calculate(data):
     expected = calculate_expected_value(data)
     dispersion = calculate_dispersion(data, expected)
+    dx, n = calculate_dx(data)
 
-    return expected, dispersion
+    return expected, dispersion, dx, n
--- lab1/main.py
+++ lab1/main.py
@@ -8,17 +8,51 @@ from model.Config import parse_config
 from DataProcessor import get_data
 from StatisticCalculator import calculate
 
+import numpy as np
+import math as mt
+import StatisticCalculator as stc
+from matplotlib import pyplot as plt
+from blume.table import table
+
 config = ''
 
 
+def show(normilize_el, bar_names, statFx, statFy):
+    fig, (ax1, ax2) = plt.subplots(2, 1,
+                       constrained_layout = True)
+    # fig.tight_layout()
+    ax1.grid(True)
+    ax1.bar(bar_names, normilize_el)
+    # plt.subplots_adjust(left=0.3, bottom=0.2)
+    # fig.tight_layout(pad=1.0)
+    tab = table(ax1, [normilize_el], loc='top', colLabels=[x.replace('\n', '') for x in bar_names],
+                rowLabels=['Эмперические частоты'])
+
+    ax2.plot(statFx, statFy, label='Стат.ф. распределения')
+    ax2.grid(True)
+    ax2.legend()
+    plt.xticks(np.arange(min(statFx), max(statFx) + 1, (max(statFx) - min(statFx)) / (len(statFx) - 1)))
+    plt.savefig("myimage.png", dpi=250)
+    plt.show()
 
 
 def do():
     print('Start', datetime.today())
     data = get_data(config)
-    print(data)
 
-    print(calculate(data))
+    expected_value, dispersion, dx, n = calculate(data)
+
+    print("\n\n\n======================================")
+    print('Expected value:  ' + str(expected_value))
+    print("dispersion:      " + str(dispersion))
+    print("dx:              " + str(dx))
+    print("number of grups: " + str(n))
+
+    counted_el, normilize_el, bar_names = stc.calculate_empheric_rule(data, dx, n)
+
+    statFx, statFy = stc.calculate_statistic_function(normilize_el, dx)
+
+    show(normilize_el, bar_names, statFx, statFy)
 
 
 # Press the green button in the gutter to run the script.
